a77a6fc8de9562dbf22102a0ad0c4682813af1ac
Kaj Olason <kajan@MacBook-Air-som-tillhor-Kaj.local>
2024-12-01 13:29:21 +0100

    Include loader-animation in the fetch-process

--- scripts/about.js
+++ scripts/about.js
@@ -1,187 +1,209 @@
-let projectData = {};
-let gitData = {};
+const workLoader = document.getElementById("workLoader");
+const educationLoader = document.getElementById("educationLoader");
 
-const res = await fetch("data/experienceData.json");
-projectData = await res.json();
-console.log(projectData);
-console.log("haha");
+function showLoader() {
+  workLoader.classList.add("showLoader");
+  educationLoader.classList.add("showLoader");
+}
 
-const response = await fetch("https://api.github.com/users/kaj-olason/repos");
-gitData = await response.json();
-console.log(gitData);
-console.log(projectData.workexperience);
-console.log("hej");
+function hideLoader() {
+  workLoader.classList.remove("showLoader");
+  educationLoader.classList.remove("showLoader");
+}
+
+function getData() {
+  showLoader();
+  fetch("data/experienceData.json")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      setTimeout(renderData, 1000);
+      function renderData() {
+        hideLoader();
+        createWorkHTMLdata(data.workexperience);
+        createEducationHTMLdata(data.education);
+      }
+    })
+    .catch((err) => console.log("fetch() failed"));
+}
+
+getData();
 
 const experienceContainer = document.getElementById("experienceContainer");
 
-projectData.workexperience.map((item) => {
-  const experienceItem = document.createElement("div");
-  experienceItem.classList.add("experienceItem");
-  experienceContainer.appendChild(experienceItem);
+const createWorkHTMLdata = (data) => {
+  data.map((item) => {
+    const experienceItem = document.createElement("div");
+    experienceItem.classList.add("experienceItem");
+    experienceContainer.appendChild(experienceItem);
 
-  const experienceItemHeader = document.createElement("div");
-  experienceItemHeader.classList.add("experienceItemHeader");
-  experienceItem.appendChild(experienceItemHeader);
+    const experienceItemHeader = document.createElement("div");
+    experienceItemHeader.classList.add("experienceItemHeader");
+    experienceItem.appendChild(experienceItemHeader);
 
-  const experienceTitle = document.createElement("p");
-  const titleNode = document.createTextNode(item.title);
-  experienceTitle.appendChild(titleNode);
-  experienceItemHeader.appendChild(experienceTitle);
+    const experienceTitle = document.createElement("p");
+    const titleNode = document.createTextNode(item.title);
+    experienceTitle.appendChild(titleNode);
+    experienceItemHeader.appendChild(experienceTitle);
 
-  const experienceSchema = document.createElement("span");
-  const schemaNode = document.createTextNode(item.schema);
-  experienceSchema.appendChild(schemaNode);
-  experienceItemHeader.appendChild(experienceSchema);
+    const experienceSchema = document.createElement("span");
+    const schemaNode = document.createTextNode(item.schema);
+    experienceSchema.appendChild(schemaNode);
+    experienceItemHeader.appendChild(experienceSchema);
 
-  const experienceItemBody = document.createElement("div");
-  experienceItemBody.classList.add("experienceItemBody");
-  experienceItem.appendChild(experienceItemBody);
+    const experienceItemBody = document.createElement("div");
+    experienceItemBody.classList.add("experienceItemBody");
+    experienceItem.appendChild(experienceItemBody);
 
-  const companyLocationContainer = document.createElement("div");
-  companyLocationContainer.classList.add("companyLocationContainer");
-  experienceItemBody.appendChild(companyLocationContainer);
+    const companyLocationContainer = document.createElement("div");
+    companyLocationContainer.classList.add("companyLocationContainer");
+    experienceItemBody.appendChild(companyLocationContainer);
 
-  const itemGroupFirst = document.createElement("div");
-  itemGroupFirst.classList.add("itemGroup");
-  companyLocationContainer.appendChild(itemGroupFirst);
+    const itemGroupFirst = document.createElement("div");
+    itemGroupFirst.classList.add("itemGroup");
+    companyLocationContainer.appendChild(itemGroupFirst);
 
-  const iconFirst = document.createElement("img");
-  itemGroupFirst.appendChild(iconFirst);
-  iconFirst.src = "/images/icons/mdi_office-building.svg";
+    const iconFirst = document.createElement("img");
+    itemGroupFirst.appendChild(iconFirst);
+    iconFirst.src = "/images/icons/mdi_office-building.svg";
 
-  const company = document.createElement("p");
-  const companyNode = document.createTextNode(item.company);
-  company.appendChild(companyNode);
-  itemGroupFirst.appendChild(company);
+    const company = document.createElement("p");
+    const companyNode = document.createTextNode(item.company);
+    company.appendChild(companyNode);
+    itemGroupFirst.appendChild(company);
 
-  const itemGroupSecound = document.createElement("div");
-  itemGroupSecound.classList.add("itemGroup");
-  companyLocationContainer.appendChild(itemGroupSecound);
+    const itemGroupSecound = document.createElement("div");
+    itemGroupSecound.classList.add("itemGroup");
+    companyLocationContainer.appendChild(itemGroupSecound);
 
-  const iconSecound = document.createElement("img");
-  itemGroupSecound.appendChild(iconSecound);
-  iconSecound.src = "/images/icons/carbon_location.svg";
+    const iconSecound = document.createElement("img");
+    itemGroupSecound.appendChild(iconSecound);
+    iconSecound.src = "/images/icons/carbon_location.svg";
 
-  const location = document.createElement("p");
-  const locationNode = document.createTextNode(item.location);
-  location.appendChild(locationNode);
-  itemGroupSecound.appendChild(location);
+    const location = document.createElement("p");
+    const locationNode = document.createTextNode(item.location);
+    location.appendChild(locationNode);
+    itemGroupSecound.appendChild(location);
 
-  const itemGroupThird = document.createElement("div");
-  itemGroupThird.classList.add("itemGroup");
-  companyLocationContainer.appendChild(itemGroupThird);
+    const itemGroupThird = document.createElement("div");
+    itemGroupThird.classList.add("itemGroup");
+    companyLocationContainer.appendChild(itemGroupThird);
 
-  const iconThird = document.createElement("img");
-  itemGroupThird.appendChild(iconThird);
-  iconThird.src = "/images/icons/carbon_location.png";
+    const iconThird = document.createElement("img");
+    itemGroupThird.appendChild(iconThird);
+    iconThird.src = "/images/icons/carbon_location.png";
 
-  const dates = document.createElement("p");
-  const datesNode = document.createTextNode(
-    `${item.startDate} - ${item.endDate}`
-  );
-  dates.appendChild(datesNode);
-  itemGroupThird.appendChild(dates);
-});
+    const dates = document.createElement("p");
+    const datesNode = document.createTextNode(
+      `${item.startDate} - ${item.endDate}`
+    );
+    dates.appendChild(datesNode);
+    itemGroupThird.appendChild(dates);
+  });
+};
 
 const educationContainer = document.getElementById("educationContainer");
 
-projectData.education.map((item) => {
-  const experienceItem = document.createElement("div");
-  experienceItem.classList.add("experienceItem");
-  educationContainer.appendChild(experienceItem);
+const createEducationHTMLdata = (data) => {
+  data.map((item) => {
+    const experienceItem = document.createElement("div");
+    experienceItem.classList.add("experienceItem");
+    educationContainer.appendChild(experienceItem);
 
-  const experienceItemHeader = document.createElement("div");
-  experienceItemHeader.classList.add("experienceItemHeader");
-  experienceItem.appendChild(experienceItemHeader);
+    const experienceItemHeader = document.createElement("div");
+    experienceItemHeader.classList.add("experienceItemHeader");
+    experienceItem.appendChild(experienceItemHeader);
 
-  const experienceTitle = document.createElement("p");
-  const titleNode = document.createTextNode(item.title);
-  experienceTitle.appendChild(titleNode);
-  experienceItemHeader.appendChild(experienceTitle);
+    const experienceTitle = document.createElement("p");
+    const titleNode = document.createTextNode(item.title);
+    experienceTitle.appendChild(titleNode);
+    experienceItemHeader.appendChild(experienceTitle);
 
-  const schemaInfoContainer = document.createElement("div");
-  schemaInfoContainer.classList.add("schemaInfoContainer");
-  experienceItemHeader.appendChild(schemaInfoContainer);
+    const schemaInfoContainer = document.createElement("div");
+    schemaInfoContainer.classList.add("schemaInfoContainer");
+    experienceItemHeader.appendChild(schemaInfoContainer);
 
-  const experienceSchema = document.createElement("span");
-  const schemaNode = document.createTextNode(item.schema);
-  experienceSchema.appendChild(schemaNode);
-  schemaInfoContainer.appendChild(experienceSchema);
+    const experienceSchema = document.createElement("span");
+    const schemaNode = document.createTextNode(item.schema);
+    experienceSchema.appendChild(schemaNode);
+    schemaInfoContainer.appendChild(experienceSchema);
 
-  const infoContainerFilter = document.createElement("div");
-  infoContainerFilter.classList.add("infoContainerFilter");
-  educationContainer.appendChild(infoContainerFilter);
+    const infoContainerFilter = document.createElement("div");
+    infoContainerFilter.classList.add("infoContainerFilter");
+    educationContainer.appendChild(infoContainerFilter);
 
-  const infoContainer = document.createElement("div");
-  infoContainer.classList.add("infoContainer");
-  infoContainerFilter.appendChild(infoContainer);
+    const infoContainer = document.createElement("div");
+    infoContainer.classList.add("infoContainer");
+    infoContainerFilter.appendChild(infoContainer);
 
-  const infoClose = document.createElement("div");
-  infoClose.classList.add("infoClose");
-  infoContainer.appendChild(infoClose);
-  const infoCloseNode = document.createTextNode("X");
-  infoClose.appendChild(infoCloseNode);
-  infoClose.addEventListener("click", () => {
-    infoContainerFilter.classList.remove("showInfo");
+    const infoClose = document.createElement("div");
+    infoClose.classList.add("infoClose");
+    infoContainer.appendChild(infoClose);
+    const infoCloseNode = document.createTextNode("X");
+    infoClose.appendChild(infoCloseNode);
+    infoClose.addEventListener("click", () => {
+      infoContainerFilter.classList.remove("showInfo");
+    });
+
+    const infoNode = document.createTextNode(item.info);
+    infoContainer.appendChild(infoNode);
+
+    const iconInfo = document.createElement("img");
+    iconInfo.src = "/images/icons/info.svg";
+    iconInfo.classList.add("iconInfo");
+    schemaInfoContainer.appendChild(iconInfo);
+    iconInfo.addEventListener("click", () => {
+      infoContainerFilter.classList.add("showInfo");
+    });
+
+    const experienceItemBody = document.createElement("div");
+    experienceItemBody.classList.add("experienceItemBody");
+    experienceItem.appendChild(experienceItemBody);
+
+    const companyLocationContainer = document.createElement("div");
+    companyLocationContainer.classList.add("companyLocationContainer");
+    experienceItemBody.appendChild(companyLocationContainer);
+
+    const itemGroupFirst = document.createElement("div");
+    itemGroupFirst.classList.add("itemGroup");
+    companyLocationContainer.appendChild(itemGroupFirst);
+
+    const iconFirst = document.createElement("img");
+    itemGroupFirst.appendChild(iconFirst);
+    iconFirst.src = "/images/icons/mdi_office-building.svg";
+
+    const company = document.createElement("p");
+    const companyNode = document.createTextNode(item.school);
+    company.appendChild(companyNode);
+    itemGroupFirst.appendChild(company);
+
+    const itemGroupSecound = document.createElement("div");
+    itemGroupSecound.classList.add("itemGroup");
+    companyLocationContainer.appendChild(itemGroupSecound);
+
+    const iconSecound = document.createElement("img");
+    itemGroupSecound.appendChild(iconSecound);
+    iconSecound.src = "/images/icons/carbon_location.svg";
+
+    const location = document.createElement("p");
+    const locationNode = document.createTextNode(item.location);
+    location.appendChild(locationNode);
+    itemGroupSecound.appendChild(location);
+
+    const itemGroupThird = document.createElement("div");
+    itemGroupThird.classList.add("itemGroup");
+    companyLocationContainer.appendChild(itemGroupThird);
+
+    const iconThird = document.createElement("img");
+    itemGroupThird.appendChild(iconThird);
+    iconThird.src = "/images/icons/carbon_location.png";
+
+    const dates = document.createElement("p");
+    const datesNode = document.createTextNode(
+      `${item.startDate} - ${item.endDate}`
+    );
+    dates.appendChild(datesNode);
+    itemGroupThird.appendChild(dates);
   });
-
-  const infoNode = document.createTextNode(item.info);
-  infoContainer.appendChild(infoNode);
-
-  const iconInfo = document.createElement("img");
-  iconInfo.src = "/images/icons/info.svg";
-  iconInfo.classList.add("iconInfo");
-  schemaInfoContainer.appendChild(iconInfo);
-  iconInfo.addEventListener("click", () => {
-    infoContainerFilter.classList.add("showInfo");
-  });
-
-  const experienceItemBody = document.createElement("div");
-  experienceItemBody.classList.add("experienceItemBody");
-  experienceItem.appendChild(experienceItemBody);
-
-  const companyLocationContainer = document.createElement("div");
-  companyLocationContainer.classList.add("companyLocationContainer");
-  experienceItemBody.appendChild(companyLocationContainer);
-
-  const itemGroupFirst = document.createElement("div");
-  itemGroupFirst.classList.add("itemGroup");
-  companyLocationContainer.appendChild(itemGroupFirst);
-
-  const iconFirst = document.createElement("img");
-  itemGroupFirst.appendChild(iconFirst);
-  iconFirst.src = "/images/icons/mdi_office-building.svg";
-
-  const company = document.createElement("p");
-  const companyNode = document.createTextNode(item.school);
-  company.appendChild(companyNode);
-  itemGroupFirst.appendChild(company);
-
-  const itemGroupSecound = document.createElement("div");
-  itemGroupSecound.classList.add("itemGroup");
-  companyLocationContainer.appendChild(itemGroupSecound);
-
-  const iconSecound = document.createElement("img");
-  itemGroupSecound.appendChild(iconSecound);
-  iconSecound.src = "/images/icons/carbon_location.svg";
-
-  const location = document.createElement("p");
-  const locationNode = document.createTextNode(item.location);
-  location.appendChild(locationNode);
-  itemGroupSecound.appendChild(location);
-
-  const itemGroupThird = document.createElement("div");
-  itemGroupThird.classList.add("itemGroup");
-  companyLocationContainer.appendChild(itemGroupThird);
-
-  const iconThird = document.createElement("img");
-  itemGroupThird.appendChild(iconThird);
-  iconThird.src = "/images/icons/carbon_location.png";
-
-  const dates = document.createElement("p");
-  const datesNode = document.createTextNode(
-    `${item.startDate} - ${item.endDate}`
-  );
-  dates.appendChild(datesNode);
-  itemGroupThird.appendChild(dates);
-});
+};
--- scripts/main.js
+++ scripts/main.js
@@ -2,80 +2,113 @@ let projectData = {};
 let gitData = {};
 let allData = [];
 
+/*
 const res = await fetch("data/experienceData.json");
 projectData = await res.json();
 console.log(projectData);
+*/
 
 const response = await fetch("https://api.github.com/users/CreateWebGit/repos");
 gitData = await response.json();
 console.log(gitData);
 
+const loader = document.getElementById("loader");
+
+function showLoader() {
+  loader.classList.add("showLoader");
+}
+
+function hideLoader() {
+  loader.classList.remove("showLoader");
+}
+
+function getData() {
+  showLoader();
+  fetch("data/experienceData.json")
+    .then((res) => {
+      return res.json();
+    })
+    .then((data) => {
+      setTimeout(renderData, 1000);
+      function renderData() {
+        hideLoader();
+        createProjectHTMLdata(data.projects);
+      }
+    })
+    .catch((err) => console.log("fetch() failed"));
+}
+
+getData();
+
 //Projects
-projectData.projects.map((projectItem) => {
-  gitData.map((gitItem) => {
-    if (projectItem.name == gitItem.name) {
-      const cardContainer = document.getElementById("cardContainer");
+function createProjectHTMLdata(data) {
+  console.log(data);
+  data.map((projectItem) => {
+    gitData.map((gitItem) => {
+      if (projectItem.name == gitItem.name) {
+        const cardContainer = document.getElementById("cardContainer");
 
-      const card = document.createElement("div");
-      card.classList.add("card");
-      cardContainer.appendChild(card);
+        const card = document.createElement("div");
+        card.classList.add("card");
+        cardContainer.appendChild(card);
 
-      const cardImage = document.createElement("div");
-      cardImage.classList.add("cardImage");
-      card.appendChild(cardImage);
-      cardImage.style.background = `url(${projectItem.image}) top no-repeat`;
-      cardImage.style.backgroundSize = "cover";
+        const cardImage = document.createElement("div");
+        cardImage.classList.add("cardImage");
+        card.appendChild(cardImage);
+        cardImage.style.background = `url(${projectItem.image}) top no-repeat`;
+        cardImage.style.backgroundSize = "cover";
 
-      const cardMain = document.createElement("div");
-      cardMain.classList.add("cardMain");
-      card.appendChild(cardMain);
+        const cardMain = document.createElement("div");
+        cardMain.classList.add("cardMain");
+        card.appendChild(cardMain);
 
-      const cardTitle = document.createElement("H2");
-      cardMain.appendChild(cardTitle);
-      const titleNode = document.createTextNode(gitItem.name);
-      cardTitle.appendChild(titleNode);
-      cardMain.appendChild(cardTitle);
+        const cardTitle = document.createElement("H2");
+        cardMain.appendChild(cardTitle);
+        const titleNode = document.createTextNode(gitItem.name);
+        cardTitle.appendChild(titleNode);
+        cardMain.appendChild(cardTitle);
 
-      const cardDescription = document.createElement("P");
-      cardMain.appendChild(cardDescription);
-      const descriptionNode = document.createTextNode(gitItem.description);
-      cardDescription.appendChild(descriptionNode);
-      cardMain.appendChild(cardDescription);
+        const cardDescription = document.createElement("P");
+        cardMain.appendChild(cardDescription);
+        const descriptionNode = document.createTextNode(gitItem.description);
+        cardDescription.appendChild(descriptionNode);
+        cardMain.appendChild(cardDescription);
 
-      const cardFooter = document.createElement("div");
-      cardFooter.classList.add("cardFooter");
-      card.appendChild(cardFooter);
+        const cardFooter = document.createElement("div");
+        cardFooter.classList.add("cardFooter");
+        card.appendChild(cardFooter);
 
-      const linkPrevContainer = document.createElement("div");
-      linkPrevContainer.classList.add("linkItem");
-      cardFooter.appendChild(linkPrevContainer);
+        const linkPrevContainer = document.createElement("div");
+        linkPrevContainer.classList.add("linkItem");
+        cardFooter.appendChild(linkPrevContainer);
 
-      const iconPrev = document.createElement("img");
-      iconPrev.src = "/images/icons/akar-icons_link-chain.png";
-      linkPrevContainer.appendChild(iconPrev);
+        const iconPrev = document.createElement("img");
+        iconPrev.src = "/images/icons/akar-icons_link-chain.png";
+        linkPrevContainer.appendChild(iconPrev);
 
-      const linkPrev = document.createElement("a");
-      const linkPrevNode = document.createTextNode("Live Preview");
-      linkPrev.appendChild(linkPrevNode);
-      linkPrev.href = gitItem.homepage;
-      linkPrevContainer.appendChild(linkPrev);
+        const linkPrev = document.createElement("a");
+        const linkPrevNode = document.createTextNode("Live Preview");
+        linkPrev.appendChild(linkPrevNode);
+        linkPrev.href = gitItem.homepage;
+        linkPrevContainer.appendChild(linkPrev);
 
-      const linkGitContainer = document.createElement("div");
-      linkGitContainer.classList.add("linkItem");
-      cardFooter.appendChild(linkGitContainer);
+        const linkGitContainer = document.createElement("div");
+        linkGitContainer.classList.add("linkItem");
+        cardFooter.appendChild(linkGitContainer);
 
-      const iconGit = document.createElement("img");
-      iconGit.src = "/images/icons/akar-icons_github-fill.png";
-      linkGitContainer.appendChild(iconGit);
+        const iconGit = document.createElement("img");
+        iconGit.src = "/images/icons/akar-icons_github-fill.png";
+        linkGitContainer.appendChild(iconGit);
 
-      const linkGit = document.createElement("a");
-      const linkGitNode = document.createTextNode("View Code");
-      linkGit.appendChild(linkGitNode);
-      linkGit.href = gitItem.html_url;
-      linkGitContainer.appendChild(linkGit);
-    }
+        const linkGit = document.createElement("a");
+        const linkGitNode = document.createTextNode("View Code");
+        linkGit.appendChild(linkGitNode);
+        linkGit.href = gitItem.html_url;
+        linkGitContainer.appendChild(linkGit);
+      }
+    });
   });
-});
+}
 
 console.log("hej");
 const cardContainer = document.getElementById("cardContainer");
